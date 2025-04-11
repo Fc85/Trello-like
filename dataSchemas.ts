2574aab27d285ade99b8c89ba6ee080d5d6da244
François-Célestin <82862772+Fc85@users.noreply.github.com>
2025-04-11 11:58:02 +0200

    Initial commit

---
+++ dataSchemas.ts
@@ -0,0 +1,33 @@
+ 
+interface Project<T extends {populateColumns: boolean} = {populateColumns: false}> {
+  _id: string,
+  name: string,
+  description: string,
+  columns: T['populateColumns'] extends true ? Column[] : string[],
+  starred?: boolean,
+  createdAt: Date,
+  updatedAt: Date,
+}
+
+interface Column<T extends {populateTasks: boolean} = {populateTasks: false}> {
+  _id: string,
+  name: string,
+  description: string,
+  color?: string,
+  tasks: T['populateTasks'] extends true ? Task[] : string[],
+  deadline?: Date,
+  createdAt: Date,
+  updatedAt: Date,
+}
+
+interface Task {
+  _id: string,
+  name: string,
+  description: string,
+  color?: string,
+  isCompleted?: boolean,
+  completedAt?: Date,
+  deadline?: Date,
+  createdAt: Date,
+  updatedAt: Date,
+}
