79c3f9d4bc0f8c20ffdd86f7542b33731524fe9b
François-Célestin <82862772+Fc85@users.noreply.github.com>
2025-04-14 11:37:10 +0200

    Ajout de la création de projet

--- app/_components/projects/CreateEditModal/CreateEditModal.tsx
+++ app/_components/projects/CreateEditModal/CreateEditModal.tsx
@@ -1,21 +1,49 @@
 import { Button } from "@/components/ui";
 import { Project } from "@/dataSchemas";
 import {X as Close} from 'lucide-react'
-
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function CreateEditModal (data: {close: () => void, project?: Project} ) {
+  const [name, setName] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
+
+  const closeModal = ():void => {
+    setName('')
+    setDescription('')
+    data.close()
+  }
+ 
+  const handleSubmit = (): void => {
+    if(name && description){
+      const projectsList: string = window.localStorage.getItem('projects') || ''
+      const formattedProjectsList: Project[] = projectsList ? JSON.parse(projectsList) : []
+      const newProject: Project = {
+        _id: uuidv4(),
+        name,
+        description,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+
+      formattedProjectsList.push(newProject)
+      
+      window.localStorage.setItem('projects', JSON.stringify(formattedProjectsList))
+      closeModal()
+    }
+  }
   
   return (
-    <div className="bg-black/40 w-full h-full absolute top-0 flex justify-center items-center cursor-pointer" onClick={()=>data.close()}>
+    <div className="bg-black/40 w-full h-full absolute top-0 flex justify-center items-center cursor-pointer" onClick={()=>closeModal()}>
       <div className="bg-white p-5 w-[500px] rounded-xl cursor-auto" onClick={(event)=>event.stopPropagation()}>
         <span className="flex justify-between items-center mb-5">
           <p>{data.project ? 'Modifier un projet' : 'Créer un projet'}</p>
-          <Close cursor='pointer' width={20} height={20} onClick={()=>data.close()} />
+          <Close cursor='pointer' width={20} height={20} onClick={()=>closeModal()} />
         </span>
         <form className="flex flex-col gap-2">
-          <input className="border-1 px-2 py-1 rounded-xl" type="text" placeholder="Nom du projet"/>
-          <textarea className="border-1 px-2 py-1 rounded-xl mb-4" placeholder="Description du projet"/>
-          <Button>{data.project ? 'Modifier' : 'Créer'}</Button>
+          <input className="border-1 px-2 py-1 rounded-xl" type="text" placeholder="Nom du projet" value={name} onChange={(e)=>setName(e.target.value)}/>
+          <textarea className="border-1 px-2 py-1 rounded-xl mb-4" placeholder="Description du projet" value={description} onChange={(e)=>setDescription(e.target.value)}/>
+          <Button onClick={handleSubmit}>{data.project ? 'Modifier' : 'Créer'}</Button>
         </form>
       </div>
     </div>
